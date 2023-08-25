11ccd47ecd0e6e2d4eec264effbb9ee3f90810c0
Ranjeet2682 <sakshemit@321>
2023-08-25 14:44:15 +0530

    Add project files.

---
+++ PMS/Controllers/ProductMasterController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PMS.Models;
+
+namespace PMS.Controllers
+{
+    public class ProductMasterController : Controller
+    {
+        private Business_Logic obj_logic;
+
+        public ProductMasterController(IConfiguration _configuration)
+        {
+            obj_logic = new Business_Logic(_configuration);
+        }
+
+
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+
+        public IActionResult UpdateProduct()
+        {
+            return View();
+        }
+
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+        
+        public IActionResult t()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CategorySearch([FromBody]cls_for_all obj)
+        {
+            DataSet dataSet = obj_logic.CategorySearch(obj.is_active, obj.spmode);
+            return Ok(dataSet);
+        }
+
+        [HttpPost]
+        public IActionResult Insert_Product([FromBody]AddProduct obj)
+        {
+           
+            DataSet dataSet = obj_logic.Insert_Product(obj.ProductName , obj.Product_Price, obj.Product_Quantity , obj.IsActive , obj.Created_By,obj.spmode);
+            return Ok(dataSet);
+        }
+
+        [HttpPost]
+        public IActionResult Update_Product([FromBody]UpdateProduct obj)
+        {
+
+            DataSet dataSet = obj_logic.Insert_Product(obj.ProductName, obj.Product_Price, obj.Product_Quantity, obj.IsActive, obj.Created_By, obj.spmode);
+            return Ok(dataSet);
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            DataSet dataSet = obj_logic.GetProduct();
+            return Ok(dataSet);
+        }
+
+
+
+
+
+
+    }
+
+}
+
+public class cls_for_all
+{
+    public int is_active { get; set; }
+    public int spmode { get; set; }
+}
+public class AddProduct
+{
+    public string ProductName;
+    public int Product_Price;
+    public int Product_Quantity;
+    public int IsActive;
+    public string Created_By;
+    public int spmode;
+
+}
+
+
+public class UpdateProduct
+{
+    public int ProductId;
+    public string ProductName;
+    public int Product_Price;
+    public int Product_Quantity;
+    public int IsActive;
+    public string Created_By;
+    public int spmode;
+
+}
+
+public class GetProduct
+{
+    public int ProductId;
+    public string ProductName;
+    public int Product_Price;
+    public int Product_Quantity;
+    public int IsActive;
+    public string Created_By;
+    public int spmode;
+
+}
---
+++ PMS/Models/Business_Logic.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using PMS.Controllers;
+using System.Reflection.Emit;
+
+namespace PMS.Models
+{
+    public class Business_Logic
+    {
+        public string objConnectionString;
+        private IConfiguration Configuration;
+        public Business_Logic(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+            objConnectionString = Configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+        }
+
+    
+        public DataSet CategorySearch(int is_active, int spmode)
+        {
+            try
+            {
+                System.Data.DataSet ds = new System.Data.DataSet();
+                Database db = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(objConnectionString);
+                DbCommand cmd = db.GetStoredProcCommand("Proc_Category_Search");
+                db.AddInParameter(cmd, "@IsActive", DbType.Int32, is_active);                
+                db.AddInParameter(cmd, "@spmode", DbType.Int32, spmode);
+                DataSet dataSet = new DataSet();
+                dataSet = db.ExecuteDataSet(cmd);
+
+                return dataSet;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public DataSet Insert_Product(string ProductName , int Product_Price, int Product_Quantity , int IsActive , string Created_By, int spmode)
+        {
+            try
+            {
+                System.Data.DataSet ds = new System.Data.DataSet();
+                Database db = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(objConnectionString);
+                DbCommand cmd = db.GetStoredProcCommand("Proc_Insert_Product");
+                db.AddInParameter(cmd, "@ProductName", DbType.String, ProductName);
+                db.AddInParameter(cmd, "@Product_Price", DbType.Int32, Product_Price);
+                db.AddInParameter(cmd, "@Product_Quantity", DbType.Int32, Product_Quantity);
+                db.AddInParameter(cmd, "@IsActive", DbType.Int32, IsActive);
+                db.AddInParameter(cmd, "@Created_By", DbType.String, Created_By);
+                db.AddInParameter(cmd, "@spmode", DbType.Int32, spmode);
+                DataSet dataSet = new DataSet();
+                dataSet = db.ExecuteDataSet(cmd);
+
+                return dataSet;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public DataSet GetProduct()
+        {
+            try
+            {
+                System.Data.DataSet ds = new System.Data.DataSet();
+                Database db = new Microsoft.Practices.EnterpriseLibrary.Data.Sql.SqlDatabase(objConnectionString);
+                DbCommand cmd = db.GetStoredProcCommand("Get_All_Products");
+             
+                db.AddInParameter(cmd, "@IsActive", DbType.Int32, 1);
+                db.AddInParameter(cmd, "@spmode", DbType.Int32, 0);
+                DataSet dataSet = new DataSet();
+                dataSet = db.ExecuteDataSet(cmd);
+
+                return dataSet;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+
+
+
+    }
+}
